b2984686604c2f638440e1e0188d132f0e95a9d7
rahma19 <56127492+rahma19@users.noreply.github.com>
2021-01-11 18:55:22 +0100

    commit

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -12,12 +12,27 @@ export class DetailComponent implements OnInit {
 name:any;
 game:any;
 currentRate = 8;
-  constructor(private activatedRoute:ActivatedRoute,private http:HttpClient) { }
+  constructor(private activatedRoute:ActivatedRoute,private http:HttpClient,private router:Router) { }
 
   ngOnInit(): void {
     this.name= this.activatedRoute.snapshot.params['name'];
     this.http.get(environment.api+"/game"+`/${this.name}`) .subscribe((res)=>{
       this.game=res;
   })
+  }
+  Onclick(){
+    this.game.rating=this.currentRate;
+    return  this.http.put(environment.api+"/game" +`/${this.name}`,this.game).subscribe(
+      (Response) => {
+            //this.msgs = [{severity:'info', summary:'Succés de modification', detail:''}];
+        console.log(this.game);
+        console.log("success");
+        this.router.navigate(['/liste']);
+      },
+        (error) =>{
+               // this.msgs = [{severity:'error', summary:'Erreur lors de la modification', detail:''}];
+         console.log("error");
+    });
+    //console.log(this.currentRate);
   }
 }
